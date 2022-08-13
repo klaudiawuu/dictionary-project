4c463cb3bd8eb950cddedd8df3799b45dd790e47
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-08-13 14:09:44 +0200

    testing netlify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dictionary app
         </a>
       </header>
     </div>
