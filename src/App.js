32f8fd39d99bea8628b476644f534d9c447bfcc6
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-09-24 12:36:43 +0200

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <img src={logo}
+        className="App-logo img-fluid"
+        alt="logo" />
       </header>
     </div>
   );
