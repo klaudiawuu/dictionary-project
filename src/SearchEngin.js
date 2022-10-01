969cb231ea2ee1135d68c3b82e7472e36f49b817
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-10-01 15:09:03 +0200

    api documenation added

--- src/SearchEngin.js
+++ src/SearchEngin.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngin.css";
 
 export default function SearchEngin() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
