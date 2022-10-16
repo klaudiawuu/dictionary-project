50deeb34b0a92c4ba5c30c58ca97c96dbe8046b2
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-10-16 23:13:07 +0200

    rendered word meanings

--- src/SearchEngin.js
+++ src/SearchEngin.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./SearchEngin.css";
+import Results from "./Results";
 
 export default function SearchEngin() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
-
+    setResults(response.data[0]);
   }
+
   function search(event) {
     event.preventDefault();
 
@@ -24,6 +27,7 @@ export default function SearchEngin() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange}></input>
       </form>
+      <Results results={results}/>
     </div>
   );
 }
